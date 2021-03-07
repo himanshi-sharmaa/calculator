94588d52b693a29fa25c359688f534e1972ac5c1
Himanshi Sharma <himanshi.sharma@hashedin.com>
2021-03-07 18:02:52 +0530

    Added basic UI and logic for calculator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const num1=[1,2,3];
+  const num2=[4,5,6];
+  const num3=[7,8,9];  
+  const op = ["Add", "Subtract", "Multiply", "Divide", "  =  "];
+  
+  const [opAdded, SetOpAdded] = useState(false);
+  const [error, setError] = useState(false)
+  const [currentOp, setCurrentOp] = useState("");
+  const [currentNumber,setCurrentNumber] = useState("0");
+  const [result, setResult] = useState("0");
+
+  const numClickEvent = (data) => { let num= currentNumber;
+    num+=data;
+    setCurrentNumber(num)
+    console.log("num", num);
+
+    if(opAdded){
+      let res = result;
+
+      // eslint-disable-next-line default-case
+      switch(currentOp){
+       case "+": 
+       res= parseInt(res)+ parseInt(num);
+           break;
+
+      case "-": 
+      res= parseInt(res) - parseInt(num);
+          break;
+
+
+      case "*":  res= parseInt(res) * parseInt(num);
+          break;
+
+
+      case "/":  num===0? setError(true): res = parseInt(res) / parseInt(num);
+          break;
+      }
+      setResult(res.toString());
+      setCurrentNumber("")
+      setCurrentOp("")
+      setCurrentOp('')
+      SetOpAdded(false)
+      console.log("Result = ", res)
+    }}
+
+    const opClickEvent = (data) => {
+      setResult(result? result: currentNumber);
+      setCurrentNumber("")
+      
+       // eslint-disable-next-line default-case
+      switch(data){
+      case "Add": SetOpAdded(true);
+          setCurrentOp("+");
+
+          break;
+
+      case "Subtract": SetOpAdded(true);
+          setCurrentOp("-");
+          break;
+      case "Multiply": SetOpAdded(true);
+          setCurrentOp("*");
+          break;
+      case "Divide": SetOpAdded(true);
+          setCurrentOp("/");
+          break;
+      } 
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+     <h1>Calculator</h1>
+     <div className="resultStyle">
+     {error ? <div>Something went wrong</div>:
+     <div className="App">{currentNumber? currentNumber: result}</div> }</div>
+     <div className="subView">
+      <div className="subNumView">
+      <div className="numContainer">{num1.map((data) => <div className="numView" key ={data} onClick={() => { numClickEvent(data) }}>{data}</div>)}</div>
+      <div className="numContainer">{num2.map((data) => <div className="numView" key ={data} onClick={() => { numClickEvent(data) }}>{data}</div>)}</div>
+      <div className="numContainer">{num3.map((data) => <div className="numView" key ={data} onClick={() => { numClickEvent(data) }}>{data}</div>)}</div>
+      </div>
+      <div className="opStyle">{op.map((data)=> <div className="operator" key={data} onClick={() => { opClickEvent(data) }}>{data}</div>)}</div>
+      </div>
+   </div>
   );
 }
 
